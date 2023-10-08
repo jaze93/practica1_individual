c375b88c5cdab370fca3e4b1b1267ee627922e49
jaze93 <146589741+jaze93@users.noreply.github.com>
2023-10-07 19:41:54 -0600

    Add files via upload

---
+++ funciones.py
@@ -0,0 +1,7 @@
+"def potencia_al_cubo(numero):" 
+"return numero ** 3" 
+"def raiz_cubica(numero):" 
+"if numero >= 0:" 
+"return numero ** (1/3)" 
+"else:" 
+"return "NUMERO NEGATIVO, RAIZ CUBICA NO VALIDA"" 
